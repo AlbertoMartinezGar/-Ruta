3100a34b87247e0a962f50a4afe81f0d1aa95238
Beto <albert.mtzz2003@gmail.com>
2019-12-07 01:53:52 -0600

    Segunda pantalla con mapa

--- app_ruta/lib/pages/home_pages.dart
+++ app_ruta/lib/pages/home_pages.dart
@@ -1,5 +1,6 @@
 // pages/home_page.dart
 import 'package:flutter/material.dart';
+import './map.dart';
 
 class HomePage extends StatelessWidget {
   final String title;
@@ -8,50 +9,57 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Column(
-        children: <Widget>[
+    
+    return new GestureDetector(
+        onTap: (){
+          Navigator.push(context, 
+            MaterialPageRoute(builder: (context) => Map())
+          );
+        },
+        child: new Container(
+          child: Column(
+            children: <Widget>[
+              Container(
+              constraints: BoxConstraints.expand(
+                height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 400.0,
+              ),
+              padding: const EdgeInsets.all(8.0),
+              color: Colors.black,
+              alignment: Alignment.center,
+              child: Text('+RUTAS',
+                style: TextStyle(
+                  fontStyle: FontStyle.normal,
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontFamily: 'Berlin Sans FB'
+                  )
+              ),
+            ),
+
           Container(
             constraints: BoxConstraints.expand(
-              height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 500.0,
+              height: Theme.of(context).textTheme.display1.fontSize * 1.1,
             ),
             padding: const EdgeInsets.all(8.0),
             color: Colors.black,
             alignment: Alignment.center,
-            child: Text('+Rutas',
+            child: Text('TOCA PARA COMENZAR',
               style: TextStyle(
-                fontStyle: FontStyle.italic,
+                fontStyle: FontStyle.normal,
                 color: Colors.white,
-                fontSize: 40,
+                fontSize: 15,
+                fontFamily: 'Berlin Sans FB'
                 )
             ),
           )
         ],
-    );
-    /*return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(this.title),
-      ),
-			body: new Container(
-        child: new Center(
-          child: new Column(
-            //mainAxisAlignment is an argument for Column, Row and other
-            // layout widgets. It does what CSS's 'justify-content' does
-            mainAxisAlignment: MainAxisAlignment.center,
-            // If you recall, Column is a layout widget that
-            // expects a List who's data is of type Widget:
-            children: <Widget>[
-              new Text(
-                'You have pushed the button this many times:',
-              ),
-              new Text('0'),
-            ],
           ),
+          
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: () => print('PRESSED!'),
-        child: new Icon(Icons.add),
-      ),
-    );*/
+        
+        
+    );
+    
   }
+
 }
